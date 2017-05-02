62abd8ce9ce948e7efe08c5501d6eb638578aede
Brian Hedberg <gemfarmer@users.noreply.github.com>
2017-05-02 11:50:13 -0500

    Update debug.rb

--- plugins/debug.rb
+++ plugins/debug.rb
@@ -6,10 +6,10 @@ module DebugFilter
     _type = value.class
     _second_type = second_value.class
     _third_type = third_value.class
-    # puts '---------------------'
-    # puts "#{value} is a #{_type}"
-    # puts '---------------------'
-    # binding.pry
+    puts '---------------------'
+    puts "#{value} is a #{_type}"
+    puts '---------------------'
+    binding.pry
   end
 end
 
